1976d7612bf13d605fc325f8ab269a25a5ec90d4
Kelly McBride <kemcbride28@gmail.com>
2021-05-03 18:14:07 -0700

    Do testing/lib refactor... everything is really bad in terms of how it works in the game. but the code is nicer.

--- lib.py
+++ lib.py
@@ -50,7 +50,7 @@ class Press:
         self._duration = duration
         self._function_name = f"press{self.num_keys}_button" if self.num_keys > 1 else "press_button"
         self._button_dirs_str = ", ".join([d.name.title() for d in self.keys])
-        self._string = f"{self._function_name}({self._button_dirs_str}, self._duration);"
+        self._string = f"{self._function_name}({self._button_dirs_str}, {self._duration});"
 
     def __str__(self):
         return self._string
@@ -124,15 +124,17 @@ def write_line(line, first_note_written, last_delay, next_delay, press_delay, pr
     line_lines = []
     if not first_note_written:
         first_note_written = True
-        delay_line = write_out_delay(last_delay)
+        if last_delay > 0:
+            line_lines.append(write_out_delay(last_delay))
     else:
-        delay_line = write_out_delay(next_delay)
+        if next_delay > 0:
+            line_lines.append(write_out_delay(next_delay))
     line = write_out_one_beat(line, press_duration=press_duration)
-    line_lines = [delay_line, line]
+    line_lines.append(line)
     return line_lines, first_note_written, press_delay
 
 
-def update_delays_empty_line(first_note_written, last_delay, next_delay, between_beat_delay):
+def update_delays(first_note_written, last_delay, next_delay, between_beat_delay):
     if not first_note_written:
         last_delay += between_beat_delay
     else:
@@ -144,6 +146,8 @@ def write_out_bar_code(bar, press_duration, bpm, last_delay=0.0):
     first_note_written = False
     between_beat_delay = get_between_beat_delay(bpm, bar.num_rows)
     press_delay = between_beat_delay - press_duration
+    full_bar_duration = get_bar_duration(bpm)
+    consumed_duration = 0
 
     next_delay = between_beat_delay - press_duration
 
@@ -151,18 +155,24 @@ def write_out_bar_code(bar, press_duration, bpm, last_delay=0.0):
     line_lines = []
     if "1" not in bar.notes:
         # Is thsi right??? IDK lol, its not like there are any empty bars.
-        return [], between_beat_delay * bar.num_rows + last_delay
+        return [Delay(full_bar_duration)], 0.0
 
     for idx, line in enumerate(bar.notes.split()):
         if "1" in line:
-            line_lines, first_note_written, next_delay = write_line(line, first_note_written, last_delay, next_delay, press_delay, press_duration)
+            line_lines, first_note_written, next_delay= write_line(line, first_note_written, last_delay, next_delay, press_delay, press_duration)
             bar_lines += line_lines
         else:
-            last_delay, next_delay = update_delays_empty_line(first_note_written, last_delay, next_delay, between_beat_delay)
+            last_delay, next_delay = update_delays(first_note_written, last_delay, next_delay, between_beat_delay)
 
-    assert(next_delay != 0.0)
-    assert(next_delay >= (between_beat_delay - press_duration))
-    return bar_lines, next_delay
+    consumed_duration += sum(line.duration for line in bar_lines)
+    remainder_delay = full_bar_duration - consumed_duration
+    # import ipdb;
+    # if remainder_delay < 0:
+    #     ipdb.set_trace()
+    bar_end_remainder_delay = write_out_delay(remainder_delay)
+    bar_lines.append(bar_end_remainder_delay)
+
+    return bar_lines, 0.0
 
 
 def produce_bar(bar):
--- testing/__main__.py
+++ testing/__main__.py
@@ -5,7 +5,7 @@ import simfile
 import lib
 
 
-class Test1(unittest.TestCase):
+class TestFullSmWrites(unittest.TestCase):
     def setUp(self):
         pass
 
@@ -21,7 +21,7 @@ class Test1(unittest.TestCase):
         press_duration = 10
         bpm = lib.get_bpm(sm)
         lines = lib.write_out_chart(chart.notes, bpm, press_duration)
-        self.assertEqual(len(lines), 0)
+        self.assertEqual(len(lines), 8)
 
     def test_single_note_song(self):
         with open("testing/test2.sm", 'r') as f:
@@ -37,18 +37,12 @@ class Test1(unittest.TestCase):
         output_lines = []
         for bar in bars:
             bar_lines, leftover_delay = lib.write_out_bar_code(bar, press_duration, bpm, leftover_delay)
-            self.assertLess(len(bar_lines), 3)
+            self.assertLess(len(bar_lines), 4)
 
         lines = lib.write_out_chart(chart.notes, bpm, press_duration)
-        self.assertEqual(len(lines), 2)
-        import ipdb; ipdb.set_trace()
+        self.assertEqual(len(lines), 9)
 
-        # I would just say let's check against the duration of the whole song, 8 bars
-        # But, how i've implemented it, it only prints a delay for up to the first
-        # note, and nothing after the last note
-        # Since it has one note at start of bar 5 (0-indexed), it will wait for that time, then 
-        # have a delay for the press duration
-        self.check_total_duration(lines, 5, bpm, addl_delay=press_duration)
+        self.check_total_duration(lines, 8, bpm)
 
     def test_simple_multi_note_song(self):
         with open("testing/test3.sm", 'r') as f:
@@ -64,18 +58,133 @@ class Test1(unittest.TestCase):
         output_lines = []
         for bar in bars:
             bar_lines, leftover_delay = lib.write_out_bar_code(bar, press_duration, bpm, leftover_delay)
-            self.assertLess(len(bar_lines), 9)
+            self.assertLess(len(bar_lines), 10)
 
         lines = lib.write_out_chart(chart.notes, bpm, press_duration)
-        self.assertEqual(len(lines), 30)
+        self.assertEqual(len(lines), 32)
 
-        # I would just say let's check against the duration of the whole song, 8 bars
-        # But, how i've implemented it, it only prints a delay for up to the first
-        # note, and nothing after the last note
-        # For this one, it prints for every bar in the song, and then technically,
-        # we expect to be missing a delay for the last "beat" - press_duration
-        between_beat_delay = lib.get_between_beat_delay(bpm, 4)
-        self.check_total_duration(lines, 8, bpm, addl_delay=-(between_beat_delay - press_duration))
+        self.check_total_duration(lines, 8, bpm)
+
+    def test_hysteria_maniac_single(self):
+        with open("testing/hysteria.sm", 'r') as f:
+            sm = simfile.load(f);
+        chart = sm.charts[2]
+        press_duration = 50
+        bpm = lib.get_bpm(sm)
+        bars = lib.read_notes(chart.notes)
+        bar_duration = lib.get_bar_duration(bpm)
+        for bar in bars:
+            bar_lines, _ = lib.write_out_bar_code(bar, press_duration, bpm)
+            self.assertEqual(sum(line.duration for line in bar_lines), bar_duration)
+            num_presses = len([p for p in bar_lines if isinstance(p, lib.Press)])
+            if isinstance(bar_lines[0], lib.Press):
+                self.assertEqual(len(bar_lines), num_presses * 2)
+            else:
+                self.assertEqual(len(bar_lines), 1 + num_presses * 2)
+
+        lines = lib.write_out_chart(chart.notes, bpm, press_duration)
+
+
+class TestBarLogic(unittest.TestCase):
+    def setUp(self):
+        self.press_duration = 10
+        self.bpm = 120.0
+        self.bar_duration = lib.get_bar_duration(self.bpm)
+
+    def test_empty_bar(self):
+        bar = lib.produce_bar("0000\n0000\n0000\n0000\n")
+        bar_duration = lib.get_bar_duration(self.bpm)
+
+        lines, _ = lib.write_out_bar_code(bar, self.press_duration, self.bpm)
+        line_duration_sum = sum(line.duration for line in lines)
+        self.assertEqual(len(lines), 1)
+        self.assertEqual(line_duration_sum, bar_duration)
+        for line in lines:
+            self.assertGreater(line.duration, 0)
+
+    def test_singlenote_bar(self):
+        bar = lib.produce_bar("0000\n0000\n0000\n1000\n")
+        bar_duration = lib.get_bar_duration(self.bpm)
+
+        lines, _ = lib.write_out_bar_code(bar, self.press_duration, self.bpm)
+        line_duration_sum = sum(line.duration for line in lines)
+        self.assertEqual(len(lines), 3)
+        self.assertIsInstance(lines[-2], lib.Press)
+        self.assertEqual(line_duration_sum, bar_duration)
+        for line in lines:
+            self.assertGreater(line.duration, 0)
+
+    def test_multinote_bar(self):
+        bar = lib.produce_bar("0010\n0100\n0001\n1000\n")
+        bar_duration = lib.get_bar_duration(self.bpm)
+
+        lines, _ = lib.write_out_bar_code(bar, self.press_duration, self.bpm)
+        line_duration_sum = sum(line.duration for line in lines)
+        self.assertEqual(len(lines), 8)
+        self.assertIsInstance(lines[0], lib.Press)
+        self.assertEqual(line_duration_sum, bar_duration)
+        for line in lines:
+            self.assertGreater(line.duration, 0)
+
+    def test_firstnoteonly_bar(self):
+        bar = lib.produce_bar("0010\n0000\n0000\n0000\n")
+        bar_duration = lib.get_bar_duration(self.bpm)
+
+        lines, _ = lib.write_out_bar_code(bar, self.press_duration, self.bpm)
+        line_duration_sum = sum(line.duration for line in lines)
+        self.assertEqual(len(lines), 2)
+        self.assertIsInstance(lines[0], lib.Press)
+        self.assertEqual(line_duration_sum, bar_duration)
+        for line in lines:
+            self.assertGreater(line.duration, 0)
+
+    def test_firstnotealternatingnotes_bar(self):
+        bar = lib.produce_bar("0010\n0000\n0100\n0000\n")
+        bar_duration = lib.get_bar_duration(self.bpm)
+
+        lines, _ = lib.write_out_bar_code(bar, self.press_duration, self.bpm)
+        line_duration_sum = sum(line.duration for line in lines)
+        self.assertEqual(len(lines), 4)
+        self.assertIsInstance(lines[0], lib.Press)
+        self.assertEqual(line_duration_sum, bar_duration)
+        for line in lines:
+            self.assertGreater(line.duration, 0)
+
+    def test_secondnotealternatingnotes_bar(self):
+        bar = lib.produce_bar("0000\n0100\n0000\n0001\n")
+        bar_duration = lib.get_bar_duration(self.bpm)
+
+        lines, _ = lib.write_out_bar_code(bar, self.press_duration, self.bpm)
+        line_duration_sum = sum(line.duration for line in lines)
+        self.assertEqual(len(lines), 5)
+        self.assertIsInstance(lines[1], lib.Press)
+        self.assertEqual(line_duration_sum, bar_duration)
+        for line in lines:
+            self.assertGreater(line.duration, 0)
+
+    def test_problem_negative_postbardelay_8countbar(self):
+        bar = lib.produce_bar("\n1000\n0000\n0010\n1000\n0100\n0000\n1000\n0000\n")
+        bar_duration = lib.get_bar_duration(self.bpm)
+
+        lines, _ = lib.write_out_bar_code(bar, self.press_duration, self.bpm)
+        line_duration_sum = sum(line.duration for line in lines)
+        self.assertEqual(len(lines), 10)
+        self.assertIsInstance(lines[0], lib.Press)
+        self.assertEqual(line_duration_sum, bar_duration)
+        for line in lines:
+            self.assertGreater(line.duration, 0)
+
+    def test_between_beat_durations_make_sense(self):
+        bar_duration = lib.get_bar_duration(self.bpm)
+
+        fourbar = lib.produce_bar("0010\n0000\n0100\n0000\n")
+        fourbetween_beat_delay = lib.get_between_beat_delay(self.bpm, fourbar.num_rows)
+
+        eightbar = lib.produce_bar("\n1000\n0000\n0010\n1000\n0100\n0000\n1000\n0000\n")
+        eightbetween_beat_delay = lib.get_between_beat_delay(self.bpm, eightbar.num_rows)
+
+        self.assertGreater(fourbetween_beat_delay, eightbetween_beat_delay)
+        self.assertEqual(fourbetween_beat_delay, 2 * eightbetween_beat_delay)
 
 
 if __name__ == '__main__':
