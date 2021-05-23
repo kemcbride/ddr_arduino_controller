ed43b87b6b2c08033238ce05402c809214ab3d35
Kelly McBride <kemcbride28@gmail.com>
2021-05-22 21:20:46 -0700

    Add builtin led blink to the presses... the controller isn't working :(

--- arduino/ps1controller/helpers.h
+++ arduino/ps1controller/helpers.h
@@ -7,7 +7,6 @@ int Up = 50;                // Up button connected to digital pin 6
 int Down = 53;              // Down button connected to digital pin 7
 int MacroPin = 10;             // Activation button connected to digital pin 10
 int PressDuration = 50; // ms
-int BetweenPressDelay = 300; // ms
 
 void press_w_delay(int button_pin, int press_duration, int post_press_delay) {
       digitalWrite(button_pin, LOW);    // Activates button
@@ -18,25 +17,31 @@ void press_w_delay(int button_pin, int press_duration, int post_press_delay) {
 
 void press_button(int button_pin, int press_duration){
       digitalWrite(button_pin, LOW);    // Activates button
+      digitalWrite(LED_BUILTIN, HIGH);
       delay(press_duration);                 // hold button for duration
       digitalWrite(button_pin, HIGH);    // De-Activates button
+      digitalWrite(LED_BUILTIN, LOW);
 }
 
 void press2_button(int button_pin, int button2_pin, int press_duration){
       digitalWrite(button_pin, LOW);    // Activates button
       digitalWrite(button2_pin, LOW);    // Activates button
+      digitalWrite(LED_BUILTIN, HIGH);
       delay(press_duration);                 // hold button for duration
       digitalWrite(button_pin, HIGH);    // De-Activates button
       digitalWrite(button2_pin, HIGH);    // Activates button
+      digitalWrite(LED_BUILTIN, LOW);
 }
 
 void press3_button(int button_pin, int button2_pin, int button3_pin, int press_duration){
       digitalWrite(button_pin, LOW);    // Activates button
       digitalWrite(button2_pin, LOW);    // Activates button
       digitalWrite(button3_pin, LOW);    // Activates button
+      digitalWrite(LED_BUILTIN, HIGH);
       delay(press_duration);                 // hold button for duration
       digitalWrite(button_pin, HIGH);    // De-Activates button
       digitalWrite(button2_pin, HIGH);    // Activates button
       digitalWrite(button3_pin, HIGH);    // Activates button
+      digitalWrite(LED_BUILTIN, LOW);
 }
 #endif
